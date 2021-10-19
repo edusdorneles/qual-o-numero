e59678a332a42cde019d898a67c991198c6b2ee3
Eduardo Dorneles <eduardo.dsdorneles@gmail.com>
2021-10-18 21:55:49 -0300

    🚀 New: Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import React from 'react';
+import Layout from './components/Layout/Layout';
+import Header from './components/Header/Header';
+
 import './App.css';
 
 function App() {
   return (
-    <div>
-      
+    <div className="general-container">
+      <Layout>
+        <Header />
+      </Layout>
     </div>
   );
 }
